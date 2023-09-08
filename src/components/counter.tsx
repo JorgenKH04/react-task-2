3f6e94cadbe0c6fefdc4bdea9d9b7a1a56f0fd07
JorgenKH04 <131149458+JorgenKH04@users.noreply.github.com>
2023-09-08 09:10:52 +0200

    Reformat counter Reformatted counter so it doesn't use callbacks

--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -3,30 +3,24 @@ import { useState } from "react";
 export function Counter() {
   const [count, setCount] = useState(0);
 
-  function incrementCount() {
-    setCount((oldCount) => oldCount + 1);
-  }
-
-  function decrementCount() {
-    setCount((oldCount) => oldCount - 1);
-  }
-
-  function resetCount() {
-    setCount(0);
-  }
-
   return (
     <div className="counter-div">
       <h2>Counter:</h2>
       <h3>{count}</h3>
       <div>
-        <button type="button" onClick={incrementCount}>
+        <button
+          type="button"
+          onClick={() => setCount((oldCount) => oldCount + 1)}
+        >
           Increment
         </button>
-        <button type="button" onClick={decrementCount}>
+        <button
+          type="button"
+          onClick={() => setCount((oldCount) => oldCount - 1)}
+        >
           Decrement
         </button>
-        <button type="button" onClick={resetCount}>
+        <button type="button" onClick={() => setCount(0)}>
           Reset counter
         </button>
       </div>
